816d422e3129e4732ed015980b31539ba3ae9baa
Claire Bergey <clairebergey@gmail.com>
2022-08-04 19:04:27 -0500

    move everything around

--- app.js
+++ app.js
@@ -11,8 +11,8 @@ const
     XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest,
     sendPostRequest = require('request').post;
 
-const port = process.env.PORT
-//const port = 6004;
+//const port = process.env.PORT
+const port = 6004;
 const researchers = ['A4SSYO0HDVD4E', 'A1BOIDKD33QSDK', 'A1MMCS8S8CTWKU','A1MMCS8S8CTWKV','A1MMCS8S8CTWKS', 'A1KXXBD1M6NBK5'];
 const blockResearcher = false;
 
